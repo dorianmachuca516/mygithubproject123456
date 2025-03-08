b779121f4889360df5b95abcbaa6b160265a1214
dorianmachuca516 <dorianmachuca516@gmail.com>
2025-03-08 20:55:25 +0000

    Add 3.js

---
+++ src/3.js
@@ -0,0 +1,8 @@
+const randomCode = () => {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let code = "";
+  for (let i = 0; i < 10; i++) {
+    code += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return code;
+};
