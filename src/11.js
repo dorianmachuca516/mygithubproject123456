bbc4163bd571be9a6515dfbb33a1544539919b9b
dorianmachuca516 <dorianmachuca516@gmail.com>
2025-03-16 04:53:47 +0000

    Add 11.js

---
+++ src/11.js
@@ -0,0 +1,7 @@
+// This is a comment in JavaScript
+function getRandomNumber(max) {
+  return Math.floor(Math.random() * max);
+}
+
+let randomNumber = getRandomNumber(10);
+console.log(randomNumber);
